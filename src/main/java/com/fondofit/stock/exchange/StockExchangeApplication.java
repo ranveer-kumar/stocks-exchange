26be800f1345d3450b9a9b2bba36418e2d6c9f21
Ranveer Kumar <ranveer.kumar@htdigital.in>
2020-07-05 23:23:22 +0530

    spring boot admin configure with kubernetes

--- src/main/java/com/fondofit/stock/exchange/StockExchangeApplication.java
+++ src/main/java/com/fondofit/stock/exchange/StockExchangeApplication.java
@@ -2,8 +2,10 @@ package com.fondofit.stock.exchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StockExchangeApplication {
 
 	public static void main(String[] args) {
